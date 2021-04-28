33a7d7d27725c5132d062f896a2a42376b1ed9eb
Alexglara <74304733+Alexglara@users.noreply.github.com>
2021-04-28 21:13:27 +0200

    UPDATE 2.7

--- trabajo-final/app.js
+++ trabajo-final/app.js
@@ -7,14 +7,6 @@ new fullpage("#fullpage", {
       const tl = new TimelineMax({ delay: 0.5 });
       tl.fromTo(title,0.5, {y: "50", opacity: 0}, {y:0, opacity:1});
 
-      if(destination.index === 1){
-         const jackets = document.querySelectorAll(".jacket");
-         const description = document.querySelector('.description');
-
-         tl.fromTo(jackets, 0.7, { x: "100%" }, { x: "-70%" })
-         .fromTo(jackets[0], 1, {opacity: 1}, {opacity: 1})
-         .fromTo(jackets[1], 1, {opacity: 0}, {opacity: 1})
-         .fromTo(jackets[2], 1, {opacity: 0}, {opacity: 1})
-      }
+      
    }
 });
