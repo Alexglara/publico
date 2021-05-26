c2f002089ac292e833aaadc7142151ed77a7f2df
Alexglara <74304733+Alexglara@users.noreply.github.com>
2021-05-26 09:31:14 +0200

    Prueba LOGIN

--- trabajo-final/inicio_de_sesion.js
+++ trabajo-final/inicio_de_sesion.js
@@ -4,7 +4,6 @@ web o pagina: ejercicios de XML y JavaScript
 autor: Prof. Carlos Boni
 fecha: 12 mayo 2021
 resumen: lectura y carga en array bidimensional de un XML
-
 sintesis:
 la lógica se basa en el uso de tres variables de tipo sessionStorage
 usuarioLogueado : si existe, su valor es el nombre de usuario en sesion
@@ -26,69 +25,76 @@ function controlar(){
 	$("#ingresar").show();
 	$("#desconectar").hide();
 			
-	if (sessionStorage.getItem("usuarioLogueado")) {
+	if (sessionStorage.getItem("usrLogueado")) {
 		// estado 3 de nuestro diagrama de estados - con usuario
 		// estamos cargando la página teniendo un usuario logueado previamente
 		// y con la sesión activa pues no se ha desconectado aún
 		// ocultamos formulario de login y mostramos desconectar
 		$("#ingresar").hide();
-		$("#desconectar").show();	
+		$("#desconectar").show();
 		
 	} else {
-		if (sessionStorage.getItem("usuarioIntentando")) {
+		//if (sessionStorage.getItem("usrIntentando")) {
 			// estado 2 de nuestro diagrama de estados - transición
 			// estamos recargando luego de que haya un intento de login
 			// debemos validar si el usuario existe
 			validarXML();
-			// tardo un poco en recargar para dar tiempo a AJAX?
-			for(let timer=1;timer<1000000;timer++);
-			location.reload();
-			
-		} else {
+		//} else {
 			// estado 1 de nuestro diagrama de estados - sin usuario
 			// mostramos formulario de login y ocultamos desconectar
-			$("#ingresar").show();
-			$("#desconectar").hide();
-		}
+			//$("#ingresar").show();
+			//$("#desconectar").hide();
+		//}
 	}
+	nombreUSR();
 }
+	function modoNoche(){
+		var element = document.getElementById("contenido");
+		element.classList.toggle("dark-mode");
+	}
+	
+	function modoNocheC(){
+		var element = document.getElementById("divCreditos");
+		element.classList.toggle("dark-mode2");
+	}
+	
+	function desconectar(){
+		sessionStorage.removeItem("usrLogueado");
+		sessionStorage.removeItem("usrIntentando");
+		sessionStorage.removeItem("claveIntentando");
+		location.reload();
+	}
 	
 	function intentar(){
-		if (typeof(Storage) !== "undefined") {
+		
 		  
 		  // oculta la opción de login 
 		  $("#ingresar").hide();
 		  
 		  // Almacena un valor usando el método setItem del objeto localStorage
-		  var x=document.forms["miFormulario"]["formUsuario"].value;
-		  var y=document.forms["miFormulario"]["formClave"].value;
-		  sessionStorage.setItem("usuarioIntentando", x);
+		  var x=document.forms["miFormulario"]["usrIntentando"].value;
+		  var y=document.forms["miFormulario"]["claveIntentando"].value;
+		  sessionStorage.setItem("usrIntentando", x);
 		  sessionStorage.setItem("claveIntentando", y);
-		  
+		  validarXML();
+		  location.reload();
+		  return;
 		  // ya tengo en memoria webStorage lo que puso en el formulario
 		  // al recargarse la página podré recordar esta información
 
-		} else {
-		  document.getElementById("mensaje").innerHTML = "Este navegador no soporta web storage...";
-		}
+		
 	}
 	
 	function validarXML() {
-		
-		// lee desde aquí.
-		usuarioIntentando=sessionStorage.getItem("usuarioIntentando");
-		claveIntentando=sessionStorage.getItem("claveIntentando");
-		sessionStorage.removeItem("usuarioIntentando");
-		sessionStorage.removeItem("claveIntentando");
-		
+	  // lee desde aquí.
 		var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				miFuncion(this);
-			}
-		};
-		xhr.open("GET", "https://carlosboniniklison.github.io/publico/ejercicios/xml/registrados.xml", true);
-		xhr.send();
+		if (this.readyState == 4 && this.status == 200) {
+		  miFuncion(this);
+		}
+	  };
+	  xhr.open("GET", "https://github.com/Alexglara/publico/blob/main/trabajo-final/validar.xml", true);
+	  xhr.send();
 	}
 
 	function miFuncion(xml) {
@@ -98,24 +104,31 @@ function controlar(){
 	  var usuario = [];
 	  var xmlDoc = xml.responseXML;
 	  var x = xmlDoc.getElementsByTagName("usuario");
-	  sessionStorage.removeItem("usuarioLogueado");
+	  sessionStorage.removeItem("usrLogueado");
 	  
 	  for (i = 0; i <x.length; i++) { 
 		// leo las etiquetas que me interesan del objeto
 		usrNom = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
 		usrPsw = x[i].getElementsByTagName("clave")[0].childNodes[0].nodeValue;
 		// actualizo la tabla de visualización
-		if ((usrNom == usuarioIntentando) && (usrPsw == claveIntentando)) {
+		if ((usrNom == sessionStorage.getItem("usrIntentando")) && (usrPsw == sessionStorage.getItem("claveIntentando"))) {
 		  // destaca el usuario que coincide con lo que buscamos
-		  sessionStorage.setItem("usuarioLogueado",usuarioIntentando);
+		  sessionStorage.setItem("usrLogueado",usrNom);
 		}
 	  }
+	  return;
 	}
-
-	function desconectar(){
-		sessionStorage.removeItem("usuarioLogueado");
-		sessionStorage.removeItem("usuarioIntentando");
-		sessionStorage.removeItem("claveIntentando");
-		location.reload();
+	
+	function nombreUSR(){
+	if(sessionStorage.usrLogueado){
+		$("#mensajeUSR").text('Bienvenido ' + sessionStorage.getItem("usrLogueado"));
+		$("#mensajeSesion").text('Cerrar Sesión');
 	}
+	else{
+		$("#mensajeUSR").text('Usuario anónimo. Inicie sesión');
+		$("#mensajeSesion").text('Iniciar Sesión');
+	}
+	
+	
+}
 	
